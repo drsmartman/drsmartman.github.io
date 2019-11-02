5e29565fa13fd5c42089ece388e11bac25d3e0f0
Marty Andolino <martyandolino@Martys-MBP.fios-router.home>
2019-11-02 00:03:30 -0400

    moved GA to head

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,7 +26,7 @@ module.exports = {
       options: {
         trackingId: "UA-67979430-1",
         // Defines where to place the tracking script - `true` in the head and `false` in the body
-        head: false,
+        head: true,
         // Setting this parameter is optional
         anonymize: true,
         // Setting this parameter is also optional
