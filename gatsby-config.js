ac12d88869126b101ab4d5dcce52cf818ad698cc
Marty Andolino <martyandolino@Martys-MacBook-Pro.local>
2019-11-03 09:59:08 -0500

    adding GTM

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,31 +7,25 @@ module.exports = {
   },
   plugins: [
     {
-          resolve: `gatsby-plugin-google-analytics`,
-          options: {
-            trackingId: "UA-67979430-1",
-            // Defines where to place the tracking script - `true` in the head and `false` in the body
-            head: true,
-            // Setting this parameter is optional
-            anonymize: true,
-            // Setting this parameter is also optional
-            respectDNT: true,
-            // Avoids sending pageview hits from custom paths
-            exclude: ["/preview/**", "/do-not-track/me/too/"],
-            // Delays sending pageview hits on route update (in milliseconds)
-            pageTransitionDelay: 0,
-            // Enables Google Optimize using your container Id
-            optimizeId: "GTM-PCJLGPB",
-            // Enables Google Optimize Experiment ID
-            //experimentId: "YOUR_GOOGLE_EXPERIMENT_ID",
-            // Set Variation ID. 0 for original 1,2,3....
-            //variationId: "YOUR_GOOGLE_OPTIMIZE_VARIATION_ID",
-            // Any additional optional fields
-            sampleRate: 5,
-            siteSpeedSampleRate: 10,
-            cookieDomain: "drsmartman.com",
-          },
-        } ,
+      resolve: "gatsby-plugin-google-tagmanager",
+      options: {
+        id: "GTM-W6FTGM",
+
+        // Include GTM in development.
+        // Defaults to false meaning GTM will only be loaded in production.
+        includeInDevelopment: false,
+
+        // datalayer to be set before GTM is loaded
+        // should be an object or a function that is executed in the browser
+        // Defaults to null
+        defaultDataLayer: { platform: "gatsby" },
+
+        // Specify optional GTM environment details.
+        //gtmAuth: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_AUTH_STRING",
+        //gtmPreview: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_PREVIEW_NAME",
+        //dataLayerName: "YOUR_DATA_LAYER_NAME",
+      },
+    },
     'gatsby-plugin-react-helmet',
     {
       resolve: `gatsby-plugin-manifest`,
